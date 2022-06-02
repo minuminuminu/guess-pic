dbbb84907c00fbf73d5605c88730b2fe02602a3a
Minu Kim <minu.kim2003@gmail.com>
2022-06-02 17:32:41 +0200

    add container + imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { Gamebox } from "./components/Gamebox";
+
+const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 250px;
+  margin: 15px;
+`;
+
 function App() {
-  return <></>;
+  const { register, handleSubmit } = useForm();
+
+  const guess = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Body>
+      <Gamebox></Gamebox>
+      <form onSubmit={handleSubmit(guess)}>
+        <Input type="text" {...register("guessStr", { required: true })} />
+        <button>Guess</button>
+      </form>
+    </Body>
+  );
 }
 
 export default App;
---
+++ src/components/Gamebox.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const GameboxContainer = styled.div`
+  width: 1000px;
+  height: 600px;
+  border: solid;
+  margin: 25px;
+`;
+
+export const Gamebox = (props) => {
+  return <GameboxContainer></GameboxContainer>;
+};
